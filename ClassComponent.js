45cbf4a3aff8cf98bb57ee8aa7e343dd86c2305c
renu28choudhary <68027594+renu28choudhary@users.noreply.github.com>
2022-02-26 18:11:21 -0500

    Add files via upload

---
+++ ClassComponent.js
@@ -0,0 +1,14 @@
+import React ,{Component} from 'react';
+
+class ClassComponent extends Component{
+    render(){
+        return(<h1>this is class component</h1>)
+    }
+}
+
+export default ClassComponent;
+
+//class component example
+
+
+
